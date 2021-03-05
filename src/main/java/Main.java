454fa7ee7af23b98cad6c0e0567ecf3a440b0a8f
dell <madsen.w5.ru@gmail.com>
2021-03-05 15:49:16 +0300

    add tests

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public String getResultFilepath(String path, int rows) {
+        String resultPath = null;
+        try {
+            File sourceFile = new File(path);
+            resultPath = sourceFile.getParent().concat("/res_").concat(sourceFile.getName());
+            BufferedReader reader = Files.newBufferedReader(sourceFile.toPath());
+            Files.deleteIfExists(Paths.get(resultPath));
+            FileWriter writer = new FileWriter(resultPath);
+            int lineSize = Files.readAllLines(sourceFile.toPath()).size();
+            if (rows > lineSize) {
+                rows = lineSize;
+            }
+            for (int i = 0; i < rows; i++) {
+                writer.write(reader.readLine().concat(System.lineSeparator()));
+            }
+            reader.close();
+            writer.close();
+        } catch (NoSuchFileException e) {
+            return "Файл отсутствует";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resultPath;
+    }
+
+    public String getResultFilepath(String path) {
+        return getResultFilepath(path, 10);
+    }
+}
--- src/test/java/SingleTest.java
+++ src/test/java/SingleTest.java
@@ -1,44 +1,40 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class SingleTest {
 
-    static String getResultFilepath(String path, int rows) {
-        String resultPath = null;
-        try {
-            File sourceFile = new File(path);
-            resultPath = sourceFile.getParent().concat("/res_").concat(sourceFile.getName());
-            List<String> outputList = new ArrayList<>();
-            List<String> sourceList = Files.lines(sourceFile.toPath()).collect(Collectors.toList());
-            IntStream.range(0, rows).forEach(i -> outputList.add(sourceList.get(i)));
-            Files.deleteIfExists(Paths.get(resultPath));
-            FileWriter writer = new FileWriter(resultPath);
-            outputList.forEach(line -> {
-                try {
-                    writer.write(line.concat(System.lineSeparator()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return resultPath;
+    Main mainTest = new Main();
+    public static final String PATH = "src/main/resources/custom-file.txt";
+
+    @Test
+    public void fileTest() {
+        String res = mainTest.getResultFilepath(PATH);
+        System.out.println(res);
     }
 
-    static String getResultFilepath(String path) {
-        return getResultFilepath(path, 10);
+    @Test
+    public void maxLinesTest() {
+        String res = mainTest.getResultFilepath(PATH, 50);
     }
 
-    public static void main(String[] args) {
-        System.out.println(getResultFilepath("src/main/resources/custom-file.txt", 10));
+    @Test
+    public void fileNotExistTest() {
+        String res = mainTest.getResultFilepath("src/main/resources/not-exist-file.txt");
+        Assert.assertEquals(res, "Файл отсутствует");
+    }
+
+    @Test
+    public void emptyFileTest() {
+        String res = mainTest.getResultFilepath("src/main/resources/empty.txt");
+    }
+
+    @Test
+    public void zeroLineTest() {
+        String res = mainTest.getResultFilepath(PATH, 0);
+    }
+
+    @Test
+    public void negativeAmountOfLinesTest() {
+        String res = mainTest.getResultFilepath(PATH, -1);
     }
 }
